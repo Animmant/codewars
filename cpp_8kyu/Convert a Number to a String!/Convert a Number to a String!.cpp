90ad00a78f7271902f4c5e84b71fdc282305f618
Roman <98784336+Animmant@users.noreply.github.com>
2024-02-17 19:09:04 +0200

    Опис змін

---
+++ cpp_8kyu/Convert a Number to a String!/Convert a Number to a String!.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+
+int positive_sum(const std::vector<int> &arrNumbers) {
+    int sumNonNegative_Numbers = 0;
+    for (int i = 0; i < arrNumbers.size(); i++){
+        if (arrNumbers[i] > 0) 
+            sumNonNegative_Numbers += arrNumbers[i];
+    }
+    return sumNonNegative_Numbers;
+}
+
+int main()
+{
+    // array initialization
+    std::vector<int> arrTransactions{1,-2,3,4,5};
+
+    int sum_income = positive_sum(arrTransactions);
+    std::cout << "Your sum: " << sum_income << std::endl;
+    return 0;
+}
