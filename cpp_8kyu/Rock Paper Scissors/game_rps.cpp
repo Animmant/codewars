045ddd299f6b7fe9cc12a6c4ace6ef2baecf0718
Roman <98784336+Animmant@users.noreply.github.com>
2024-03-13 16:56:29 +0000

    rename

---
+++ cpp_8kyu/Rock Paper Scissors/game_rps.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <stdlib.h>
+#include <fstream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+string rps(const std::string& p1, const std::string& p2){
+    switch(){
+        case 
+    }
+    return "d";
+}
+
+int main(){
+    string firstPlayer, secondPlayer;
+    cout << rps(firstPlayer, secondPlayer) << endl;
+}
