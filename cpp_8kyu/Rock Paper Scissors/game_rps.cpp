1557f89274415056c6df08ca74299150b5fac11b
Roman <98784336+Animmant@users.noreply.github.com>
2024-03-17 18:24:18 +0000

    finish

--- cpp_8kyu/Rock Paper Scissors/game_rps.cpp
+++ cpp_8kyu/Rock Paper Scissors/game_rps.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <string>
-#include <stdlib.h>
-#include <fstream>
-#include <cmath>
 #include <string>
 
-using namespace std;
 
-string rps(const std::string& p1, const std::string& p2){
-    if(&p1){
-        return "Player 1 won!";
+
+std::string rps(const std::string& p1, const std::string& p2){
+    if(p1 == p2){
+        return "Draw!";
     }
-    return "d";
+
+    if ((p1 == "scissors" && p2 == "paper") ||
+        (p1 == "paper" && p2 == "rock") ||
+        (p1 == "rock" && p2 == "scissors")) {
+        return "Player 1 won!";
+    } else {
+        return "Player 2 won!";
+    }
 }
 
 int main(){
-    string firstPlayer, secondPlayer;
-    cout << "Player 1, enter your choice (rock, paper, scissors): ";
-    cin >> firstPlayer;
-    cout << "Player 2, enter your choice (rock, paper, scissors): ";
-    cin >> secondPlayer;
+    std::string firstPlayer, secondPlayer;
+    std::cout << "Player 1, enter your choice (rock, paper, scissors): ";
+    std::cin >> firstPlayer;
+    std::cout << "Player 2, enter your choice (rock, paper, scissors): ";
+    std::cin >> secondPlayer;
 
-    cout << rps(firstPlayer, secondPlayer) << endl;
+    std::cout << rps(firstPlayer, secondPlayer) << std::endl;
 }
