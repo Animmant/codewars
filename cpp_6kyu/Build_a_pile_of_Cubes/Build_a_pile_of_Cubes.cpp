3c8ba888b9e4f76675fd16abb2b6e8959adfc4c8
Roman <98784336+Animmant@users.noreply.github.com>
2024-03-26 00:54:20 +0200

    test copilot Asum

--- cpp_6kyu/Build_a_pile_of_Cubes/Build_a_pile_of_Cubes.cpp
+++ cpp_6kyu/Build_a_pile_of_Cubes/Build_a_pile_of_Cubes.cpp
@@ -1,11 +1,12 @@
+#include <cmath>
+
+
 class ASum{
 public:
     static long long findNb(long long m)
     {
-        long long n = 0;
-        for (long long sum = 0; sum < m; ++n)
-            sum += n * n * n;
-        return (n * n * n == m) ? (n - 1) : -1;
+        // n = (-1 + sqrt(1 + 8m)) / 2
+        
     }
 };
 
