07f3b2a3b94c79225742acf9f6fa188bbbc55684
Roman <98784336+Animmant@users.noreply.github.com>
2024-03-27 13:45:48 +0200

    cubes_pipe

--- cpp_6kyu/Build_a_pile_of_Cubes/Build_a_pile_of_Cubes.cpp
+++ cpp_6kyu/Build_a_pile_of_Cubes/Build_a_pile_of_Cubes.cpp
@@ -5,8 +5,15 @@ class ASum{
 public:
     static long long findNb(long long m)
     {
-        // n = (-1 + sqrt(1 + 8m)) / 2
-        
+        // a^3 + (a-1)^3 + (a-2)^3 + ... + 1^3 = m = s^2= (n * (n + 1) / 2)^2
+        long long D = 1 + 8 * sqrt(m); // n^2 + n -2 * sqrt(m) = 0
+        if (D < 0) return -2;
+        long long n = (-1 + sqrt(D)) / 2;
+        long long total = (n * (n + 1) / 2) * (n * (n + 1) / 2);
+        if (total == m) {
+            return n;
+        }
+        return -1;
     }
 };
 
@@ -15,7 +22,10 @@ public:
 int main()
 {
     std::cout << ASum::findNb(1071225) << std::endl;          // Outputs: 45
-    std::cout << ASum::findNb(91716553919377) << std::endl;   // Outputs: -1
+    std::cout << ASum::findNb(24723578342962) << std::endl;   // Outputs: -1
+    std::cout << ASum::findNb(135440716410000) << std::endl;  
+    std::cout << ASum::findNb(40539911473216) << std::endl;  
+    std::cout << ASum::findNb(26825883955641) << std::endl;  
 
     return 0;
 }
