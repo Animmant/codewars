96691dd320874366ce45df3eb978dcab2e5f6090
Roman <98784336+Animmant@users.noreply.github.com>
2024-04-03 14:25:25 +0000

    startDuration

---
+++ cpp_4kyu/Human readable duration format/readableDuration.cpp
@@ -0,0 +1,22 @@
+#include <string>
+
+std::string format_duration(int seconds) {
+    int amountValues = seconds;
+    while (/* condition */)
+    {
+        /* code */
+    }
+    
+}
+
+#include <iostream>
+
+int main() {
+    auto result714 = format_duration(714);
+    std::cout << result714 << std::endl;
+
+    auto result714 = format_duration(3665);
+    std::cout << result714 << std::endl;
+
+    return 0;
+}
