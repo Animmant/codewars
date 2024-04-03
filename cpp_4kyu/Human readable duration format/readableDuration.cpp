ca48c32f811c649adc3c4716271bad56bd9165bd
Roman <98784336+Animmant@users.noreply.github.com>
2024-04-03 15:07:03 +0000

    start 8ky

--- cpp_4kyu/Human readable duration format/readableDuration.cpp
+++ cpp_4kyu/Human readable duration format/readableDuration.cpp
@@ -1,12 +1,25 @@
 #include <string>
 
 std::string format_duration(int seconds) {
-    int amountValues = seconds;
-    while (/* condition */)
-    {
-        /* code */
-    }
+    if(seconds)
+
+
+
+    int years = seconds / (365 * 24 * 60 * 60);
+    seconds %= (365 * 24 * 60 * 60);
+    int days = seconds / (24 * 60 * 60);
+    seconds %= (24 * 60 * 60);
+    int hours = seconds / (60 * 60);
+    seconds %= (60 * 60);
+    int minutes = seconds / 60;
+    seconds %= 60;
+
+    std::string sec = std::to_string(seconds % 60) + " second",
+                minute = std::to_string((seconds / 60) % 60) + " minute",
+                hour = std::to_string((seconds / 3600) % 60) + " hour";
     
+    switch()
+
 }
 
 #include <iostream>
@@ -15,8 +28,8 @@ int main() {
     auto result714 = format_duration(714);
     std::cout << result714 << std::endl;
 
-    auto result714 = format_duration(3665);
-    std::cout << result714 << std::endl;
+    auto resultmany = format_duration(3665);
+    std::cout << resultmany << std::endl;
 
     return 0;
 }
