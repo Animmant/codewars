3a60f8ba5ae05afb8251f89a2633d6a84452cf82
Roman <98784336+Animmant@users.noreply.github.com>
2024-04-07 20:26:41 +0000

    finishmaxsubarray

--- cpp_5kyu/Maximum subarray sum/maxsubarray.cpp
+++ cpp_5kyu/Maximum subarray sum/maxsubarray.cpp
@@ -10,9 +10,9 @@ int sumArraySelected(const std::vector<int>& arr, int start, int end){
 
 int maxSequence(const std::vector<int>& arr){
   int max =0;
-  for(int i = 1; i <= arr.size(); i++){
-    for(int j = 0; j < i ; j++){
-      int sumCurrentSubArray = sumArraySelected(arr, j, arr.size() -i + j);
+  for(int i = 0; i < arr.size(); i++){
+    for(int j = 0; j <= i ; j++){
+      int sumCurrentSubArray = sumArraySelected(arr, j, arr.size() - i + j);
       if(max < sumCurrentSubArray){
         max = sumCurrentSubArray;
       }
@@ -21,12 +21,29 @@ int maxSequence(const std::vector<int>& arr){
   return max;
 }
 
+int dmaxSequence(const vector<int>& arr) {
+	int localmax=0;
+	int globalmax=0;
+	for (int i = 0;i<arr.size();i++)
+	{
+		localmax = localmax + arr[i];
+		if (localmax > globalmax)
+			globalmax = localmax;
+		if (localmax < 0)
+			localmax = 0;
+	}
+	return globalmax;
+}
+
 #include <iostream>
 
 int main() {
-    auto result = maxSequence({-2, 1, -3, 4, -1, 2, 1, -5, 4});
+    auto result = dmaxSequence({-2, 1, -3, 4, -1, 2, 1, -5, 4});
     std::cout << result << std::endl;
-
+    auto chell = maxSequence({7,4,11,-11,39,36,10,-6,37,-10,-32,44,-26,-34,43,43});
+    std::cout << chell << std::endl;
+    auto chell2 = sumArraySelected({7,4,11,-11,39,36,10,-6,37,-10,-32,44,-26,-34,43,43}, 0, 16);
+    std::cout << chell2 << std::endl;
 
     return 0;
 }
